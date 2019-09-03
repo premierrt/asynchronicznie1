3c613e13cc6ead1f914415bc74ef2e54a854caa4
Rafał Tyszewski <rafal.tyszewski@play.pl>
2019-09-03 12:09:12 +0200

    v1

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
+import async.callback.EventListenerWithCallbackFunction;
+import async.callback.EventListenerWithCallbackFunctionImpl;
+import async.callback.KlasaZWatkiemGlownym;
+
 public class Main {
+    public static void main(){
+        KlasaZWatkiemGlownym glowny = new KlasaZWatkiemGlownym();
+        EventListenerWithCallbackFunction callback = new EventListenerWithCallbackFunctionImpl();
+        glowny.setEventListener(callback);
+        glowny.metodaGlowna();
+
+    }
 }
--- src/async/callback/KlasaZWatkiemGlownym.java
+++ src/async/callback/KlasaZWatkiemGlownym.java
@@ -4,4 +4,26 @@ package async.callback;
  *
  */
 public class KlasaZWatkiemGlownym {
+
+    private EventListenerWithCallbackFunction eventListener;
+    public void metodaGlowna(){
+        System.out.println("===Metoda Główna === To jest metoda glowna ");
+
+        Runnable runnable = () ->{
+          System.out.println(">>>Wątek do wyokonania zadania async");
+        };
+        Thread t1 = new Thread(runnable);
+        t1.start();
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("===Metoda Główna === Tutaj konczy sie metoda glowna");
+    }
+
+    public void setEventListener(EventListenerWithCallbackFunction eventListener) {
+        this.eventListener = eventListener;
+    }
 }
